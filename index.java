4eba7cbefee25dc003b903c4f02288f6d8395a97
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-17 23:55:38 +0600

    auto commit Sat Jan 17 23:55:38 +06 2026

--- index.java
+++ index.java
@@ -5,6 +5,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
+        max_profit = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int profit = prices[j] - prices[j];
+            }
+        }
 
     }
 }
