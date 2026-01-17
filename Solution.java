eaf1b00d02d7acb575b8a9412242fdafb244708d
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-18 00:07:53 +0600

    auto commit Sun Jan 18 00:07:53 +06 2026

--- Solution.java
+++ Solution.java
@@ -19,5 +19,6 @@ public class Solution {
 public static void main(String[] args) {
     Solution solution = new Solution();
     // test data
-    int[] prices={7,1,5,3,6,4}
+    int[] prices = { 7, 1, 5, 3, 6, 4 }; // arr
+
 }
