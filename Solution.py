e7f25af8912a66fef4c6effc92142cc7f20a22a1
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-19 00:30:12 +0600

    auto commit Mon Jan 19 00:30:12 +06 2026

--- Solution.py
+++ Solution.py
@@ -16,8 +16,9 @@ class Solution:
 
         for i in range(n - 2):
             for j in range(i + 1, n - 1):
-                if nums[i] + nums[j] + nums[k] == 0:
-                    triplet = sorted([nums[i], nums[j], nums[k]])
-                    if triplet not in result:
-                        result.append(triplet)
+                for k in range(j + 1, n):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        triplet = sorted([nums[i], nums[j], nums[k]])
+                        if triplet not in result:
+                            result.append(triplet)
         return result
