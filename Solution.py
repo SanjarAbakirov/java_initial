cd8df19a225de468b238bedd72766f37e423891c
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-19 00:23:20 +0600

    auto commit Mon Jan 19 00:23:20 +06 2026

--- Solution.py
+++ Solution.py
@@ -3,3 +3,16 @@
 #              n = 100 -> 100 * 100 * 100 = 1 000 000
 
 # ex. leetcode
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[List[int]]]:
+        #  cube complexity
+        n = len(nums)
+        result = []
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                if nums[i] + nums[j] + nums[k] == 0:
+                    triplet = sorted([nums[i], nums[j], nums[k]])
+                    if triplet not in result:
+                        result.append(triplet)
+        return result
