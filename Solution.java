76c2284e61a5fdf42249b417b08a37f39e64e183
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-19 22:33:35 +0600

    auto commit Mon Jan 19 22:33:35 +06 2026

--- Solution.java
+++ Solution.java
@@ -34,7 +34,10 @@ import java.until.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(Sysytem.in);
+        java.util.Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();
 
     }
 }
