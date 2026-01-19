3c1811c9412a7d0dced23ba8e25e119427649bb6
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-19 22:28:29 +0600

    auto commit Mon Jan 19 22:28:29 +06 2026

--- BinaryWorld.java
+++ BinaryWorld.java
@@ -1,6 +1,6 @@
 public class BinaryWorld {
     public static void main(String[] args) {
-        int decimalDigit = 7;
+        int decimalDigit = 5;
         String binaryCode = String.format("%4s", Integer.toBinaryString(decimalDigit)).replace(' ', '0');
         System.out.println("The number " + decimalDigit + " in BCD: " + binaryCode);
 
