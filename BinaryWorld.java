33f3c6d5e3558d13ba8dcdf677ccc888ae4d3a60
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-19 22:40:44 +0600

    auto commit Mon Jan 19 22:40:44 +06 2026

--- BinaryWorld.java
+++ BinaryWorld.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+
+ 
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();
+        System.out.println("Username is: " + userName);
+
+   
+
+
+
 public class BinaryWorld {
     public static void main(String[] args) {
+
+
+
+
+
         int decimalDigit = 5;
         String binaryCode = String.format("%4s", Integer.toBinaryString(decimalDigit)).replace(' ', '0');
         System.out.println("The number " + decimalDigit + " in BCD: " + binaryCode);
