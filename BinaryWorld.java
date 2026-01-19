b54233fdca30418db66e5b94dbeaac6d3ae69b79
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-19 22:52:59 +0600

    auto commit Mon Jan 19 22:52:59 +06 2026

--- BinaryWorld.java
+++ BinaryWorld.java
@@ -4,7 +4,7 @@ public class BinaryWorld {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your number");
+        System.out.println("=".repeat(40));
 
         Integer decimalDigit = myObj.nextLine();
 
