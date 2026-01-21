40b94e8db7f0d2861195a54ba0ba1de79cdabb3f
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-21 19:17:44 +0600

    auto commit Wed Jan 21 19:17:44 +06 2026

--- main.py
+++ main.py
@@ -2,4 +2,9 @@
 # O(n^2) quadratic complexity
 
 # Ex braking ice among friends
+# n = 5, 5 * 4/2 = 10
+# n = 10, 10 * 9 / 2 = 45
+
+#  n * (n - 1) / 2 = n^2 - n/2 = n^2
+# divide by 2 because we have doubled actions inside of equation that is no needed.
 #
