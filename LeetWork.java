04b0d9c30325dcc693abde3149c83e4f02535e2c
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-21 23:35:24 +0600

    auto commit Wed Jan 21 23:35:24 +06 2026

--- LeetWork.java
+++ LeetWork.java
@@ -15,3 +15,11 @@
 // Input: nums = [-1,0,3,5,9,12], target = 2
 // Output: -1
 // Explanation: 2 does not exist in nums so return -1
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+    }
+}
+;
