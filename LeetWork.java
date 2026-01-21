5610c88eaf2ff40c24c22c8b9da082cb03cd3a41
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-21 23:54:30 +0600

    auto commit Wed Jan 21 23:54:30 +06 2026

--- LeetWork.java
+++ LeetWork.java
@@ -18,8 +18,8 @@
 
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        int left = 0; // the left part of research - from 0 index
+        int right = nums.length - 1; //
 
         while (left <= right) {
             int mid = left + (right - left) / 2; // safely defined middle
