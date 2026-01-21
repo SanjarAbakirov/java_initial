8a703047a558a3df3af4923848f10b14c0615cac
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-21 19:20:47 +0600

    auto commit Wed Jan 21 19:20:47 +06 2026

--- main.py
+++ main.py
@@ -7,4 +7,12 @@
 
 #  n * (n - 1) / 2 = n^2 - n/2 = n^2
 # divide by 2 because we have doubled actions inside of equation that is no needed.
-#
+# n - i we optimize algorithms with sorted elements by the end of the list
+# and - 1 means we want to keep counter inside of the array
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):  # (0, 5)
+        for j in range(0, n - i - 1):  # (0, 4)
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
