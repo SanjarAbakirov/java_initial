887df67e231eb5f04f1472571f9e9ac1d23ed013
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-23 23:28:04 +0600

    auto commit Fri Jan 23 23:28:04 +06 2026

--- LeetWork.java
+++ LeetWork.java
@@ -38,7 +38,27 @@
 //         }
 //         return -1; // not found
 //     }
+// ---------------
 
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    // -------
     public static void main(String[] args) {
         Solution s = new Solution();
 
@@ -54,23 +74,3 @@
 }
 
 // dop reshenie - razberem
-// class Solution {
-// public int search(int[] nums, int target) {
-// int start=0;
-// int end=nums.length-1;
-// while(start<=end){
-// int mid = start + (end- start)/2;
-// if(nums[mid] == target){
-// return mid;
-// }
-// else if(nums[mid]<target){
-// start=mid+1;
-// }
-// else if(nums[mid] > target){
-// end=mid-1;
-// }
-
-// }
-// return -1;
-// }
-// }
