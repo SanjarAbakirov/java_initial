a7ddf7e47269a27b40b49e59b8c4627f1e7ee40f
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-24 00:05:38 +0600

    auto commit Sat Jan 24 00:05:38 +06 2026

--- LeetWork.java
+++ LeetWork.java
@@ -79,7 +79,6 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if (s == null || t == null) {
             System.out.println("One of the strings is equalls null");
-            return;
         }
 
         int len1 = s.length();
