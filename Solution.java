508d0a1dd6f2d779b642d4c5e38fee8b82b8905e
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-25 00:00:52 +0600

    auto commit Sun Jan 25 00:00:52 +06 2026

--- Solution.java
+++ Solution.java
@@ -1,30 +1,30 @@
-// public class Solution {
-// public int maxProfit(int[] prices) {
-// int n = prices.length;
-// int max_profit = 0;
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int max_profit = 0;
 
-// for (int i = 0; i < n; i++) {
-// for (int j = i + 1; j < n; j++) {
-// int profit = prices[j] - prices[i];
-// if (profit > max_profit) {
-// max_profit = profit;
-// }
-// }
-// }
-// return max_profit;
-// }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > max_profit) {
+                    max_profit = profit;
+                }
+            }
+        }
+        return max_profit;
+    }
 
-// // method for testing
-// public static void main(String[] args) {
-// Solution solution = new Solution();
-// // test data
-// int[] prices = { 7, 1, 5, 9, 3, 6, 4 }; // arr
+    // method for testing
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // test data
+        int[] prices = { 7, 1, 5, 9, 3, 6, 4 }; // arr
 
-// int result = solution.maxProfit(prices);
-// System.out.println("maximum profit: " + result);
-// }
+        int result = solution.maxProfit(prices);
+        System.out.println("maximum profit: " + result);
+    }
 
-// }
+}
 
 // TC O(n^2)
 // SC O(1)
