8f35e9b9ce1e0c14fbbad8067046543940e7165d
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-24 23:31:53 +0600

    auto commit Sat Jan 24 23:31:53 +06 2026

--- LeetWork.java
+++ LeetWork.java
@@ -38,38 +38,38 @@ class LeetWork {
         }
         return -1; // not found
     }
+
+    // ---------------
+
+    // class Solution {
+    // public int search(int[] nums, int target) {
+    // int start = 0;
+    // int end = nums.length - 1;
+
+    // while (start <= end) {
+    // int mid = start + (end - start) / 2;
+    // if (nums[mid] == target) {
+    // return mid;
+    // } else if (nums[mid] < target) {
+    // start = mid + 1;
+    // } else if (nums[mid] > target) {
+    // end = mid - 1;
+    // }
+    // }
+    // return -1;
+    // }
+
+    // // -------
+    public static void main(String[] args) {
+        LeetWork s = new LeetWork();
+
+        // Здесь пишешь свои данные
+        int[] nums = { -1, 0, 2, 3, 5, 9, 12 };
+
+        int target = 3;
+
+        // Вызываешь метод search
+        int result = s.search(nums, target);
+        System.out.println("Результат: " + result);
+    }
 }
-// ---------------
-
-// class Solution {
-// public int search(int[] nums, int target) {
-// int start = 0;
-// int end = nums.length - 1;
-
-// while (start <= end) {
-// int mid = start + (end - start) / 2;
-// if (nums[mid] == target) {
-// return mid;
-// } else if (nums[mid] < target) {
-// start = mid + 1;
-// } else if (nums[mid] > target) {
-// end = mid - 1;
-// }
-// }
-// return -1;
-// }
-
-// // -------
-// public static void main(String[] args) {
-// Solution s = new Solution();
-
-// // Здесь пишешь свои данные
-// int[] nums = { -1, 0, 3, 5, 9, 12 };
-
-// int target = 2;
-
-// // Вызываешь метод search
-// int result = s.search(nums, target);
-// System.out.println("Результат: " + result);
-// }
-// }
