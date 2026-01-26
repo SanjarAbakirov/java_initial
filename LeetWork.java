bc4ef0c27992e15d50d3545e724d5aad16ebaef4
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-26 21:36:21 +0600

    auto commit Mon Jan 26 21:36:21 +06 2026

--- LeetWork.java
+++ LeetWork.java
@@ -1,1 +1,8 @@
 // -------- Contains duplicate -----
+// int array nums -> if any value appears at leaste twice in the array
+// return true and oppiste false
+// find available dublicates
+
+// input nums = [1, 2, 3, 4, 1]
+// Output: true
+// indices 0 and 3
