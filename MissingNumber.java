acb16d60c2fa3aef416fbefc9d1ff98faf2e81f9
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-27 22:38:49 +0600

    auto commit Tue Jan 27 22:38:49 +06 2026

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,18 +1,18 @@
 class MissingNumber {
-    public int missingNumber(int[] nums) {
-        int result = 0;
-        // XOR all indexs (0 ..n)
-        for (int i = 0; i <= nums.length; i++) {
-            result ^= i;
-        }
-    // XOR for all number from array
-    for(
-
-    int num:nums)
-    {
-        result ^= num;
+    public static void main(String[] args) {
+          public int missingNumber(int[] nums) {
+                int result = 0;
+                // XOR all indexs (0 ..n)
+                for (int i = 0; i <= nums.length; i++) {
+                    result ^= i;
+                }
+                // XOR for all number from array
+                for (int num : nums) {
+                    result ^= num;
+                }
+                // finally it will be the last num
+                return result;
+            }
     }
-    // finally it will be the last num
-    return result;
 
 }
