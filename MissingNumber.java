7f9834aa22e111b43674d3a838302b0ab9e7dd81
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-28 20:42:02 +0600

    fixing syntax

--- MissingNumber.java
+++ MissingNumber.java
@@ -4,7 +4,7 @@ public class MissingNumber {
 
         // XOR all indexs (0 ..n)
         for (int i = 0; i <= nums.length; i++) {
-            result ^= i;
+            result ^= i; // bite comparison
         }
 
         // XOR for all number from array
@@ -21,9 +21,9 @@ public class MissingNumber {
 
         // Tests
         int[] test1 = { 3, 0, 1 };
-        System.out.println("Test 1: " + solver.missingNumber(test1)); // 3
+        System.out.println("Test 1: " + solver.missingNumber(test1)); // 2
         int[] test2 = { 0, 2, 1, 4 };
-        System.out.println("Test 2: " + solver.missingNumber(test2)); // 2
+        System.out.println("Test 2: " + solver.missingNumber(test2)); // 3
         int[] test3 = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
         System.out.println("Test 3: " + solver.missingNumber(test3)); // 8
     }
