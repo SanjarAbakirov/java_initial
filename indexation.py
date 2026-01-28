bc9afcbbf225fcb41e93afbb90e6c851cda642a8
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-28 20:49:20 +0600

    auto commit Wed Jan 28 20:49:20 +06 2026

--- indexation.py
+++ indexation.py
@@ -3,6 +3,6 @@ numbers = [5, 10, 15]
 sum_positives = 0
 
 for num in numbers:
-if num > 0:
-sum_positives += num
+    if num > 0:
+        sum_positives += num
 print("Сумма положительных:", sum_positives)
