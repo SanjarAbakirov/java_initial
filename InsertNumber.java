2b83fddf5ce87d9d47fae56e6e65c4b1676ef27c
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-29 21:31:55 +0600

    auto commit Thu Jan 29 21:31:55 +06 2026

---
+++ InsertNumber.java
@@ -0,0 +1,48 @@
+// TC O(n^2)
+// SC O(1)
+
+// --------- input first iteration ---------
+import java.util.Scanner; // библиотека - сканнер
+
+public class InsertNumber {
+
+        // class означает директория - Main имя директории
+        public static void main(String[] args) {
+
+
+
+    }
+    
+}
+
+
+
+// // public - публичный доступ все могут заходить
+// // main - главный контейнер
+// // static - не двигается. не нужно создавать объект
+// // void - ничего не возвращает
+// // String[] args - аргументы командной строки
+// // это все большая кнопка запуска
+// Scanner myObj = new Scanner(System.in); // Scanner - это как тип
+инструмента
+// // myObj - имя инструмента
+// // Scanner() - конструктор - инструкция по сборке
+// // Sysytem.in - входная дверь в команту - откуда читать
+
+// // по сути я говорю - Сканнер мой объект равно новый Сканер в нем
+// системный вход
+// создай нового
+// рабочего на заводе
+
+// System.out.println("Enter username");
+
+// String userName = myObj.nextLine();
+// // myObj - имя субъекта (наш Scanner)
+// // .nextLine() - метод прочитай строку
+
+// System.out.println("Username is: " + userName);
+// // System - система (весь компьютер)
+// // .out - куда выводить
+// // .println - Напечатай строку (print line)
+// }
+// }
