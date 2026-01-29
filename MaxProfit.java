b2c3603aa8bde33e2a57e2a52e2559f0322a82ff
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-29 21:42:02 +0600

    auto commit Thu Jan 29 21:42:02 +06 2026

--- MaxProfit.java
+++ MaxProfit.java
@@ -17,7 +17,6 @@ public class MaxProfit {
     // method for testing
     public static void main(String[] args) {
         Solution solution = new Solution();
-        // test data
         int[] prices = { 7, 1, 5, 9, 3, 6, 4 };
         int result = solution.maxProfit(prices);
         System.out.println("maximum profit: " + result);
