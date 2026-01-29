b3c0a9b25114660d5b2a9d642326e204b5d02eba
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-29 22:26:30 +0600

    auto commit Thu Jan 29 22:26:30 +06 2026

--- MaxProfit.java
+++ MaxProfit.java
@@ -25,16 +25,49 @@
 //     }
 // }
 
-// ---------- 121 Max_profit solutuon #2------
+// ---------- 121 Max_profit solutuon #2-----O(n)-
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         if (prices.length == 0)
+//             return 0;
+
+//         int min_price = prices[0];
+//         int max_profit = 0;
+
+//         for (int i = 1; i < prices.length; i++) {
+//             if (prices[i] < min_price) {
+//                 min_price = prices[i];
+//             }
+
+//             else if (prices[i] - min_price > max_profit) {
+//                 max_profit = prices[i] - min_price;
+//             }
+//         }
+//         return max_profit;
+//     }
+
+//     // method for testing
+//     public static void main(String[] args) {
+//         Solution maxProfit = new Solution();
+
+//         int[] prices = { 7, 1, 5, 3, 6, 4 };
+//         int result = maxProfit.maxProfit(prices);
+//         System.out.println("maximum profit: " + result);
+//     }
+// }
+
+// --------- 121 Max Profit -------- Shabdan----O(n)-
 class Solution {
     public int maxProfit(int[] prices) {
         if (prices.length == 0)
             return 0;
 
+        int n = prices.length;
         int min_price = prices[0];
         int max_profit = 0;
 
-        for (int i = 1; i < prices.length; i++) {
+        for (int i = 1; i < n; i++) {
+            // sell day
             if (prices[i] < min_price) {
                 min_price = prices[i];
             }
