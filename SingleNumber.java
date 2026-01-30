92139b4693b7f2043c9a29d92161527232cc0be5
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-31 00:00:48 +0600

    adde single nums

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,9 +1,17 @@
 public class SingleNumber {
-    public int result = 0;nums=[1,2,3,4,2,1,3]
+    public int singleNUmber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
 
-    for(
-    int i = 0;i<nums.length;i++)
-    {
-        result ^= nums[i];
-    }return result;
+    public static void main(int[] nums) {
+
+        int[] nums1 = { 1, 2, 3, 4, 2, 1, 3 };
+
+        // System.out.println("maximum profit: " + result);
+    }
+
 }
