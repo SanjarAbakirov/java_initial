441ddb7dc6b665c918007b284ada72bedb8e8d27
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-01-31 00:08:44 +0600

    auto commit Sat Jan 31 00:08:44 +06 2026

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,5 +1,5 @@
 public class SingleNumber {
-    public int singleNUmber(int[] nums) {
+    public int singleNumber(int[] nums) {
         int result = 0;
         for (int i = 0; i < nums.length; i++) {
             result ^= nums[i];
@@ -8,7 +8,7 @@ public class SingleNumber {
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        SingleNumber solution = new SingleNumber();
 
         // Тесты
         int[] test1 = { 2, 2, 1 };
