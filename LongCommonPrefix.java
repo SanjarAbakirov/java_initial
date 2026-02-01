0152b68797e77d3ffbe6712a540e398f49f9782a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-01 19:05:41 +0600

    auto commit Sun Feb  1 19:05:41 +06 2026

--- LongCommonPrefix.java
+++ LongCommonPrefix.java
@@ -1,3 +1,12 @@
 public class LongCommonPrefix {
-    public String longestCommonPrefix(String[] strs)
+    public String longestCommonPrefix(String[] strs) {
+        // Edge case: empty array
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        // Edge case: array with fulled array
+        if (strs.length == 1) {
+            return strs[0];
+        }
+    }
 }
