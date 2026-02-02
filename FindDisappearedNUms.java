7b9ff15d5f758434d9eb8e69754fa673c28f8b99
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-02 22:32:14 +0600

    auto commit Mon Feb  2 22:32:14 +06 2026

--- FindDisappearedNUms.java
+++ FindDisappearedNUms.java
@@ -4,5 +4,11 @@ import java.util.List;
 import java.util.Set;
 
 public class FindDisappearedNUms {
-
+    public List<Integer> FindDisappearedNUms(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+    }
 }
