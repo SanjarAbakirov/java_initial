d8bdf33d4433db0a1d87d010d3f0d789914a8781
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-03 20:55:33 +0600

    auto commit Tue Feb  3 20:55:33 +06 2026

--- BigOexponent.java
+++ BigOexponent.java
@@ -4,5 +4,17 @@ public class BigOexponent {
     public static void main(String[] args) {
         String[] objects = { "triangle", "rectangle", "circle" };
         List<List<String>> allSets = findAllSets(objects);
+
+        System.out.println("All possible sets of objects");
+        for (<List<String>> set : allSet)
+            System.out.println("All sets: " + findAllSets.size());
+        // space complexity O(2 ^ n)
+    
+    public static List<List<String>>findAllSets(String[] objects) {
+        List<List<String>>result = new ArrayList<>();
+        // begin from empty
+        result.add(new ArrayList<>());
+    }
+
     }
 }
