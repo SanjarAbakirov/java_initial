f28606bb469574eb3fea5aff0752379f49cc3715
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-03 20:51:30 +0600

    auto commit Tue Feb  3 20:51:30 +06 2026

--- BigOexponent.java
+++ BigOexponent.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 public class BigOexponent {
     public static void main(String[] args) {
-
+        String[] objects = { "triangle", "rectangle", "circle" };
+        List<List<String>> allSets = findAllSets(objects);
     }
 }
