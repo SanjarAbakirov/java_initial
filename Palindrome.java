044d421ae863082f3a4de9686e4b873c3c513afa
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-03 22:27:00 +0600

    auto commit Tue Feb  3 22:27:00 +06 2026

--- Palindrome.java
+++ Palindrome.java
@@ -6,6 +6,16 @@ public class Palindrome {
     }
 
     public static void main(String[] args) {
-
+        String[] testCases {
+            "racecar",           // true - простое слово
+            "A man a plan a canal Panama", // true - фраза
+            "hello",             // false - не палиндром
+            "12321",             // true - число как строка
+            "Madam, I'm Adam",   // true - с пунктуацией
+        };
+        for (String test : testCases) {
+            boolean result = isPalindrome2(test);
+            System.out.printf("\"%s\" → %s%n", test, result ? "Палиндром ✅" : "Не палиндром ❌");
+        }
     }
 }
