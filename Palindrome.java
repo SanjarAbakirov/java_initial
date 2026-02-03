4c6b21b68b3892e4fca104852bc3966ac4af44d8
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-03 22:20:56 +0600

    auto commit Tue Feb  3 22:20:56 +06 2026

---
+++ Palindrome.java
@@ -0,0 +1,9 @@
+public class Palindrome {
+    public boolean isPalindrome(String s) {
+        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        return cleaned.equals(reversed);
+
+    }
+}
+}
