b7edbf53899daf42afb80ada7bb497297a5b9d7c
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-05 19:50:48 +0600

    auto commit Thu Feb  5 19:50:48 +06 2026

--- ClimbStairs.java
+++ ClimbStairs.java
@@ -2,19 +2,30 @@
 public class ClimbStairs {
 
     // -----way #1------------
+    // public int climbStairs(int n) {
+    // int[] dp = new int[n + 1];
+    // return helper(n, dp);
+    // }
+
+    // public int helper(int n, int[] dp) {
+    // if (n == 0 || n == 1) {
+    // return 1;
+    // }
+    // if (dp[n] != 0) {
+    // return dp[n];
+    // }
+    // return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+    // }
+
+    // -------way # 2 ---------------
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
-        return helper(n, dp);
-    }
-
-    public int helper(int n, int[] dp) {
-        if (n == 0 || n == 1) {
-            return 1;
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        if (dp[n] != 0) {
-            return dp[n];
-        }
-        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+        return dp[n];
     }
 
     public static void main(String[] args) {
@@ -23,8 +34,8 @@ public class ClimbStairs {
         System.out.println("Testing task 'Climbing stairs': ");
         System.out.println("=".repeat(40));
 
-        int[] testCases = { 1, 2, 3, 4, 5, 6, 10 };
-        int[] expected = { 1, 2, 3, 4, 8, 13, 89 };
+        int[] testCases = { 1, 2, 3, 4, 5, 6, 10, 45 };
+        int[] expected = { 1, 2, 3, 4, 8, 13, 89, 90 };
 
         for (int i = 0; i < testCases.length; i++) {
             int n = testCases[i];
