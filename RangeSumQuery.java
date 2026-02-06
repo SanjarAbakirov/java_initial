55c109fb55911ba7cc448351bba6d68f0a3f70be
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-06 23:38:35 +0600

    auto commit Fri Feb  6 23:38:35 +06 2026

--- RangeSumQuery.java
+++ RangeSumQuery.java
@@ -1,26 +1,14 @@
 public class RangeSumQuery {
+    private int[] prefixSum;
 
-    private int[] prefixSum;3 4
-
-    public NumArray(int[] nums) { // prefix summs array
-5        prefixSum = new int[nums.length + 1]; 
-6
-7        for (int i = 0; i < nums.length; i++){
-8            prefixSum[i + 1] = prefixSum[i] + nums[i];
-9        }
-10  
-
-
-}
-
-    11
-
-    12
+    public NumArray(int[] nums) {
+        prefixSum = new int[nums.length + 1];
+            for (int i = 0; i < nums.length; i++){
+                prefixSum[i + 1] = prefixSum[i] + nums[i];
+            }  
+        }
 
     public int sumRange(int left, int right) {
-13        return prefixSum[right + 1] - prefixSum[left];
-14        
-15    
-}16
-
+        return prefixSum[right + 1] - prefixSum[left];
+    }
 }
