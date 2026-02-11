868447680a6c75199f7b45229ca7b9accd75f03e
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-11 23:30:47 +0600

    auto commit Wed Feb 11 23:30:47 +06 2026

--- NumOfOneBits.java
+++ NumOfOneBits.java
@@ -13,11 +13,31 @@ public class NumOfOneBits {
         return count;
     }
 
-    public static void main(int args) {
-        int n = 11;
-    }
+   public static void main(String[] args) {
+        int[] testNumbers = {
+            0,          // двоично: 0 -> 0 единиц
+            1,          // 1 -> 1
+            2,          // 10 -> 1
+            3,          // 11 -> 2
+            4,          // 100 -> 1
+            5,          // 101 -> 2
+            7,          // 111 -> 3
+            8,          // 1000 -> 1
+            11,         // 1011 -> 3 (пример из условия)
+            128,        // 10000000 -> 1
+            255,        // 11111111 -> 8
+            256,        // 100000000 -> 1
+            Integer.MAX_VALUE // 2147483647, 31 единица
+        };
 
-}
+        System.out.println("Проверка функции hammingWeight:\n");
+        for (int n : testNumbers) {
+            int bits = hammingWeight(n);
+            System.out.printf("n = %-12d (binary: %32s) -> set bits: %d%n",
+                              n, Integer.toBinaryString(n), bits);
+        }
+
+
 
 // public static int hammingWeight(int n) {
 // int count = 0;
