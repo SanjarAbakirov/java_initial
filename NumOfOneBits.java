4880d5954991f107c21461c8cf0cc0183bc5b595
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-11 23:24:43 +0600

    auto commit Wed Feb 11 23:24:43 +06 2026

--- NumOfOneBits.java
+++ NumOfOneBits.java
@@ -1,11 +1,11 @@
+// -------191. Number of 1 Bits-----------
+
 public class NumOfOneBits {
     public int hammingWeight(int n) {
         return Integer.bitCount(n);
     }
 }
 
-public static void main(String[] args) { // main is needed for start
-    int[] nums1 = { 1, 7, 3, 4, 6, 2, 7 };
-    boolean result = containsDuplicate(nums1);
-    System.out.println("The duplicate is " + result);
+public static void main(int args) {
+    int n = 11;
 }
