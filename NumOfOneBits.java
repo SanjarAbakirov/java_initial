8fdb1abd46eeed20c2f3765e9ec41cb162b816c8
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-11 23:20:40 +0600

    auto commit Wed Feb 11 23:20:40 +06 2026

--- NumOfOneBits.java
+++ NumOfOneBits.java
@@ -3,3 +3,9 @@ public class NumOfOneBits {
         return Integer.bitCount(n);
     }
 }
+
+public static void main(String[] args) { // main is needed for start
+    int[] nums1 = { 1, 7, 3, 4, 6, 2, 7 };
+    boolean result = containsDuplicate(nums1);
+    System.out.println("The duplicate is " + result);
+}
