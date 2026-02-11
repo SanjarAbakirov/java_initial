e0356a914a8be2720a2b66c3e2ad0b13bcde9fae
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-11 23:26:44 +0600

    auto commit Wed Feb 11 23:26:44 +06 2026

--- NumOfOneBits.java
+++ NumOfOneBits.java
@@ -4,8 +4,18 @@ public class NumOfOneBits {
     public int hammingWeight(int n) {
         return Integer.bitCount(n);
     }
+
+    public static void main(int args) {
+        int n = 11;
+    }
+
 }
 
-public static void main(int args) {
-    int n = 11;
-}
+// public static int hammingWeight(int n) {
+// int count = 0;
+// while (n != 0) {
+// n &= (n - 1);
+// count++;
+// }
+// return count;
+// }
