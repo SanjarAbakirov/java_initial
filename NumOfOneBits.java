22485d7f606c444dc9bf1855d382415046049535
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-11 23:28:45 +0600

    auto commit Wed Feb 11 23:28:45 +06 2026

--- NumOfOneBits.java
+++ NumOfOneBits.java
@@ -1,8 +1,16 @@
 // -------191. Number of 1 Bits-----------
 
 public class NumOfOneBits {
-    public int hammingWeight(int n) {
-        return Integer.bitCount(n);
+    // public int hammingWeight(int n) {
+    // return Integer.bitCount(n);
+    // }
+
+    public static int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) { // int в Java — 32 бита
+            count += (n >> i) & 1;
+        }
+        return count;
     }
 
     public static void main(int args) {
