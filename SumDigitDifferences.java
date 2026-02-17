6b153c32213d81a9afaa243329b2acfcae34488a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-18 00:26:31 +0600

    auto commit Wed Feb 18 00:26:31 +06 2026

--- SumDigitDifferences.java
+++ SumDigitDifferences.java
@@ -28,36 +28,36 @@ public class SumDigitDifferences {
     // return totalSum;
     // }
 
-    // ----------------------O(k)
+    // ----------O(k)
 
-    public long sumDigitDifferences(int[] nums) {
-    int n = nums.length;
-    if (n < 2) return 0;
-    int d = String.valueOf(nums[0]).length();
-    Берём первое число, превращаем его в строчку и смотрим длину – это количество
-    цифр (например, 13 → длина 2).
+    public long sumOfDigitDifferences(int[] nums) {
+        int n = nums.length;
+        if (n < 2)
+            return 0;
+        int d = String.valueOf(nums[0]).length();
+        // Берём первое число, превращаем его в строчку и смотрим (например, 13 → длина
+        // 2)
 
-    ----
+        // ----
 
-    long total = 0;
-    for (int pos = 0; pos < d; pos++) {
-    int[] freq = new int[10];
-    int divisor = (int) Math.pow(10, d - 1 - pos); // для извлечения цифры на
-    позиции pos
-    for (int num : nums) {
-    int digit = (num / divisor) % 10;
-    freq[digit]++;
-    }
-    long totalPairs = (long) n * (n - 1) / 2;
-    long samePairs = 0;
-    for (int cnt : freq) {
-    if (cnt > 1) {
-    samePairs += (long) cnt * (cnt - 1) / 2;
-    }
-    }
-    total += totalPairs - samePairs; // пары с разными цифрами на этой позиции
-    }
-    return total;
+        long total = 0;
+        for (int pos = 0; pos < d; pos++) {
+            int[] freq = new int[10];
+            int divisor = (int) Math.pow(10, d - 1 - pos); // для извлечения цифры на позиции pos
+            for (int num : nums) {
+                int digit = (num / divisor) % 10;
+                freq[digit]++;
+            }
+            long totalPairs = (long) n * (n - 1) / 2;
+            long samePairs = 0;
+            for (int cnt : freq) {
+                if (cnt > 1) {
+                    samePairs += (long) cnt * (cnt - 1) / 2;
+                }
+            }
+            total += totalPairs - samePairs; // пары с разными цифрами на этой позиции
+        }
+        return total;
     }
 
     public static void main(String[] args) {
