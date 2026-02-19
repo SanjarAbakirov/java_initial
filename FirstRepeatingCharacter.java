5aa5df719d95abd0cedc67843e887f204add124f
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-19 21:56:55 +0600

    auto commit Thu Feb 19 21:56:55 +06 2026

---
+++ FirstRepeatingCharacter.java
@@ -0,0 +1,18 @@
+public class FirstRepeatingCharacter {
+    public static char firstLetterToAppearTwice(String s) {
+        boolean[] seen = new boolean[26]; // для отслеживания встреченных букв
+        for (char c : s.toCharArray()) {
+            int index = c - 'a'; // получаем индекс буквы (0-25)
+            if (seen[index]) {
+                return c; // если буква уже встречалась, возвращаем её
+            }
+            seen[index] = true; // помечаем как встреченную
+        }
+        return ' '; // по условию задачи всегда есть повторяющаяся буква
+    }
+
+    public static void main(String[] args) {
+        String s = "vbbghkk";
+        System.out.println("Первая повторяющаяся буква: " + firstLetterToAppearTwice(s)); // ожидается 'c'
+    }
+}
