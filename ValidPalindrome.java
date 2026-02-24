e88711ca023001cdeff6f4784d113f93ac26287c
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-24 20:20:36 +0600

    auto commit Tue Feb 24 20:20:36 +06 2026

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -1,19 +1,19 @@
 public class ValidPalindrome {
-    StringBuilder builder = new StringBuilder();for(
-    char ch:s.toCharArray())
-    {
-        if (Character.isLetterOrDigit(ch)) {
-            builder.append(Character.toLowerCase(ch));
+    public boolean isPalindrome(String s) {
+        StringBuilder builder = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (Character.isLetterOrDigit(ch)) {
+                builder.append(Character.toLowerCase(ch));
+            }
         }
+        String cleaned = builder.toString();
+        String reversed = builder.reverse().toString();
+        return cleaned.equals(reversed);
     }
-    String cleaned = builder.toString();
-    String reversed = builder.reverse().toString();return cleaned.equals(reversed);
 
-    public class Main {
-        public static void main(String[] args) {
-            ValidPalindrome vp = new ValidPalindrome();
-            System.out.println(vp.isPalindrome("A man, a plan, a canal: Panama")); // true
-            System.out.println(vp.isPalindrome("race a car")); // false
-        }
+    public static void main(String[] args) {
+        ValidPalindrome vp = new ValidPalindrome();
+        System.out.println(vp.isPalindrome("A man, a plan, a canal: Panama")); // true
+        System.out.println(vp.isPalindrome("race a car")); // false
     }
 }
