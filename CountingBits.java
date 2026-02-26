e0deb7417b3015b0ee0ba92bffa7fdcaf64f165a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-26 23:40:46 +0600

    auto commit Thu Feb 26 23:40:46 +06 2026

---
+++ CountingBits.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class CountingBits {
+    class Solution {
+        public int[] countBits(int n) {
+            int[] ans = new int[n + 1];
+            for (int i = 0; i <= n; i++) {
+                ans[i] = Integer.bitCount(i);
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String args) {
+
+    }
+}
