e45bd3961d721314cf7e58ccb77fc29b4d3cc747
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-28 17:31:40 +0600

    auto commit Sat Feb 28 17:31:40 +06 2026

--- CountPairsLessThenTarget.java
+++ CountPairsLessThenTarget.java
@@ -4,8 +4,15 @@ public class CountPairsLessThenTarget {
         int count = 0;
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
-
+                int sum = nums[i] + nums[j];
+                if (sum < target) {
+                    sum += count;
+                }
             }
         }
+    }
+
+    public static void main(String[]){
+        
     }
 }
