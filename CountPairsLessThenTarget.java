882c9c0fe32a0ee5db8e54af97bd4b84e9e72961
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-02-28 18:38:26 +0600

    auto commit Sat Feb 28 18:38:26 +06 2026

--- CountPairsLessThenTarget.java
+++ CountPairsLessThenTarget.java
@@ -7,12 +7,12 @@ import java.util.Arrays;
 public class CountPairsLessThenTarget {
     public int countPairs(List<Integer> nums, int target) {
         int count = 0;
+
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
-                int sum = nums[i] + nums[j];
-                if (sum < target) {
-                    sum += count;
-                }
+                int sum = nums.get(i) + nums.get(j);
+                if (sum < target)
+                    count++;
             }
         }
     }
