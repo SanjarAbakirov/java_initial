59a8f58c548b74ba5f0bda091d77130a976bee1d
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-02 01:03:03 +0600

    auto commit Mon Mar  2 01:03:03 +06 2026

--- RemoveDuplicatesFromSortedArr.java
+++ RemoveDuplicatesFromSortedArr.java
@@ -1,7 +1,13 @@
 public class RemoveDuplicatesFromSortedArr {
     public int removeDuplicates(int[] nums) {
-        int k = nums.length;
-        for (int i = 0; i < k; i++){
-            
+        if (nums.length == 0) return 0;
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
         }
+        return nums[j];
 }
