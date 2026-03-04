8d670d5318d6b653a1c7292a26b27c751d5aa2af
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-04 07:26:53 +0600

    auto commit Wed Mar  4 07:26:53 +06 2026

--- ExponentEx.java
+++ ExponentEx.java
@@ -2,5 +2,10 @@ import java.util.*;
 
 public class ExponentEx {
     // -------O(2^n)------
-
+    public static void main(String[] args) {
+        // ------three objects-----
+        String[] objects = { "triangle", "cube", "circul" };
+        // Lookin for all possible options
+        List<List<String>> allSets = findAllSets(objects);
+    }
 }
