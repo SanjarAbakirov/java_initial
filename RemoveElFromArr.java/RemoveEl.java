baad6d6338cc2104f8cdbf28a1c871606b828fbf
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-06 01:38:31 +0600

    auto commit Fri Mar  6 01:38:31 +06 2026

--- RemoveElFromArr.java/RemoveEl.java
+++ RemoveElFromArr.java/RemoveEl.java
@@ -4,7 +4,8 @@ public class RemoveEl {
     public int removeElement(int[] nums, int val) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
-
+            if(nums[i] == val) {
+                nums.removeElement(nums, nums[i])}
         }
     }
 
