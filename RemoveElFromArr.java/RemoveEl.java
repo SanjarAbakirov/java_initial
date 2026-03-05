1e8960611de624b3a34a85fafd678e52a43af8fe
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-06 01:14:02 +0600

    auto commit Fri Mar  6 01:14:02 +06 2026

--- RemoveElFromArr.java/RemoveEl.java
+++ RemoveElFromArr.java/RemoveEl.java
@@ -6,9 +6,9 @@ public class RemoveEl {
     }
 
     public static void main(String[] args) {
-        int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
-        MajorityElement me = new MajorityElement();
-        int result = me.majorityElement(nums);
+        int[] nums = { 2, 2, 1, 1, 1 };
+        RemoveEl me = new RemoveEl();
+        int result = me.removeElement(nums);
         System.out.println("Majority element is: " + result);
     }
 
