6f783d445ec93d36092d9631d4c10a995a227e24
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-06 23:35:04 +0600

    auto commit Fri Mar  6 23:35:04 +06 2026

--- RomanNumbers.java
+++ RomanNumbers.java
@@ -9,10 +9,8 @@ public class RomanNumbers {
         romanMap.put('X', 10);
         romanMap.put('L', 50);
         romanMap.put('C', 100);
-
-        String C = 100;
-        String D = 500;
-        String M = 1000;
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
 
         int total = 0;
         int length = s.length();
