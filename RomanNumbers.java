de53227562fc9644956a109e9e268e2710d5543a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-06 23:02:47 +0600

    auto commit Fri Mar  6 23:02:47 +06 2026

--- RomanNumbers.java
+++ RomanNumbers.java
@@ -1,9 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumbers {
     public int romanToInt(String s) {
 
     }
 
     public void main(String[] args) {
+        Map<Character, Integer> romanMap = new HashMap<>();
         String I = 1;
         String V = 5;
         String X = 10;
