4e1262f8e1e8d9c5b6a57550885237fd85fac030
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-07 23:42:07 +0600

    auto commit Sat Mar  7 23:42:07 +06 2026

--- PlusOne.java
+++ PlusOne.java
@@ -1,6 +1,20 @@
 public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+
     public static void main(String[] args) {
-        PlusOneFn plusOne = new PlusOne();
+        PlusOneFn plusOne = new PlusOneFn();
         int[] digits = { 1, 2, 3 };
         System.out.println();
 
