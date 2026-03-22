1cca76e4694783bad35d0ba7f18593ad5772c593
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-23 00:06:20 +0600

    auto commit Mon Mar 23 00:06:20 +06 2026

--- ConvertArrToBinaryTree.java
+++ ConvertArrToBinaryTree.java
@@ -1,16 +1,34 @@
-Сlass ConvertArrToBinaryTree {
-private class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+Сlass ConvertArrToBinaryTree{
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
     }
 }
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
 
+    private TreeNode buildBST(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        // находим середину, чтобы дерево было сбалансированным
+        int mid = left + (right - left) / 2;
+        // создаём узел из среднего элемента
+        TreeNode root = new TreeNode(nums[mid]);
+        // рекурсивно строим левую и правую части
+        root.left = buildBST(nums, left, mid - 1);
+        root.right = buildBST(nums, mid + 1, right);
+        return root;
+    }
+}
 
 }
