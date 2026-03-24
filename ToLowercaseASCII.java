808628264d0abe1dcb9f6b353a712008e3ef10d5
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-24 23:45:30 +0600

    auto commit Tue Mar 24 23:45:30 +06 2026

--- ToLowercaseASCII.java
+++ ToLowercaseASCII.java
@@ -5,19 +5,22 @@ public class ToLowercaseASCII {
         System.out.println(lower); // hello
     }
 
-    public static String toLowerCaseAscii(String input) {
-        if (input == null)
-            return null;
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            // Проверяем, находится ли символ в диапазоне 'A'..'Z' (ASCII 65..90)
-            if (ch >= 'A' && ch <= 'Z') {
-                // Преобразуем в строчную букву: разница между кодами 'a' и 'A' равна 32
-                ch = (char) (ch + 32);
-            }
-            result.append(ch);
-        }
-        return result.toString();
+    public static String toLowerCaseAscii(String s) {
+        // if (input == null)
+        // return null;
+        // StringBuilder result = new StringBuilder();
+        // for (int i = 0; i < input.length(); i++) {
+        // char ch = input.charAt(i);
+        // // Проверяем, находится ли символ в диапазоне 'A'..'Z' (ASCII 65..90)
+        // if (ch >= 'A' && ch <= 'Z') {
+        // // Преобразуем в строчную букву: разница между кодами 'a' и 'A' равна 32
+        // ch = (char) (ch + 32);
+        // }
+        // result.append(ch);
+        // }
+        // return result.toString();
+
+        String lower = s.toLowerCase();
+        return lower;
     }
 }
