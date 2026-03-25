2c4e5fe3f3941d59494430c3ba2f0fcfa0da814b
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-25 22:55:31 +0600

    auto commit Wed Mar 25 22:55:31 +06 2026

--- Add_binary.java
+++ Add_binary.java
@@ -1,11 +1,12 @@
 class Add_binary {
-    public String addBinary(String argss) {
-        String a = "11";
-        String b = "1";
+    public static void main(String[] args) {
+        String a = "1001";
+        String b = "111";
         int sum1 = Integer.parseInt(a, 2);
         int sum2 = Integer.parseInt(b, 2);
+
         int sum = sum1 + sum2;
         String result = Integer.toBinaryString(sum);
-        return result;
+        System.out.println("the result is: " + result);
     }
 }
