c5a2e08437a7def27876607021c393bd08c11cff
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-30 00:16:06 +0600

    auto commit Mon Mar 30 00:16:06 +06 2026

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -3,43 +3,43 @@ import java.util.Arrays;
 class MoveZeroes {
     // =====solution 1======
     // O(n) because two pointer is moving only to the right
-    // public void moveZeroes(int[] nums) {
-    // int l = 0;
-    // int r = 0;
-    // int n = nums.length;
-    // while (r < n) {
-    // while (r < n && nums[r] == 0) {
-    // r++;
-    // }
-    // // if (r == l) {
-    // // break;
-    // // }
-    // // nums[r] != 0;
-    // nums[l] = nums[r];
-    // // if l = 0 we will rewrite it to l's place again
-    // l++;
-    // r++;
-    // }
-    // while (l > n) {
-    // nums[l] = 0;
-    // }
-    // l++;
-    // }
-
-    // ===== Solution 2 =====
     public void moveZeroes(int[] nums) {
         int l = 0;
-        // points to position to place next non-zero
-        for (int r = 0; r < nums.length; r++) {
-            if (nums[r] != 0) {
-                // swap non-zero element with the left pointer
-                int temp = nums[l];
-                nums[l] = nums[r];
-                nums[r] = temp;
-                l++;
+        int r = 0;
+        int n = nums.length;
+        while (r < n) {
+            while (r < n && nums[r] == 0) {
+                r++;
             }
+            if (r == l) {
+                break;
+            }
+            // nums[r] != 0;
+            nums[l] = nums[r];
+            // if l = 0 we will rewrite it to l's place again
+            l++;
+            r++;
         }
+        while (l > n) {
+            nums[l] = 0;
+        }
+        l++;
     }
+
+    // ===== Solution 2 =====
+    // public void moveZeroes(int[] nums) {
+    // int l = 0;
+    // // points to position to place next non-zero
+    // for (int r = 0; r < nums.length; r++) {
+    // if (nums[r] != 0) {
+    // // swap non-zero element with the left pointer
+    // int temp = nums[l];
+    // nums[l] = nums[r];
+    // nums[r] = temp;
+    // l++;
+    // }
+    // }
+    // }
 
     public static void main(String[] args) {
         // 1. Create test input
