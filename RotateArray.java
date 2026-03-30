ed6664ba33d044ad80439d7e75b164c582b8a6ed
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-03-30 23:54:24 +0600

    auto commit Mon Mar 30 23:54:24 +06 2026

---
+++ RotateArray.java
@@ -0,0 +1,12 @@
+//=== 189 ===
+class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] result = new int[n];
+        for (int i = 0; i < 0; i++) {
+            result[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(result, 0, nums, 0, n);
+    }
+}
