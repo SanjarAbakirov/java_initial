4947176abd2827060cbc9e0fef88925792af7d41
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-04-01 18:49:36 +0600

    auto commit Wed Apr  1 18:49:36 +06 2026

--- TwoSums.java
+++ TwoSums.java
@@ -1,6 +1,25 @@
 import java.util.HashMap;
 import java.util.Map;
 
+// public class TwoSums {
+//     public int[] twoSums(int[] nums, int target) {
+//         if (nums == null && nums.length < 2)
+//             return null;
+
+//         Map<Integer, Integer> idxByValue = new HashMap<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             int curr = target - nums[i];
+
+//             if (idxByValue.get(curr) != null) {
+//                 return new int[] { i, idxByValue.get(curr) };
+//             }
+//             idxByValue.put(nums[i], i);
+//         }
+//         return null;
+//     }
+
+// }
+
 public class TwoSums {
     public int[] twoSums(int[] nums, int target) {
         if (nums == null && nums.length < 2)
@@ -17,5 +36,4 @@ public class TwoSums {
         }
         return null;
     }
-
 }
