9184711085cce4e2813eff0fd18c1d2492da4304
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-04-08 23:05:13 +0600

    auto commit Wed Apr  8 23:05:13 +06 2026

---
+++ RuRnningSum_of_1d_Array.java
@@ -0,0 +1,9 @@
+public class RuRnningSum_of_1d_Array {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4 };
+
+    }
+}
+
+// Output:[1,3,6,10]
+// Explanation: Running sum is obtained as follows:[1,1+2,1+2+3,1+2+3+4]
