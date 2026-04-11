8b70d55edd13dc73cfa636823e84601ec7acd401
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-04-11 23:29:01 +0600

    auto commit Sat Apr 11 23:29:01 +06 2026

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,25 +1,62 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class FizzBuzz {
+// public class FizzBuzz {
+//     public List<String> fizzBuzz(int n) {
+//         List<String> answer = new ArrayList<>(n);
+
+//         for (int i = 1; i <= n; i++) {
+//             boolean divisibleBy3 = i % 3 == 0;
+//             boolean divisibleBy5 = i % 5 == 0;
+
+//             if (divisibleBy3 && divisibleBy5) {
+//                 answer.add("FizzBuzz");
+//             } else if (divisibleBy3) {
+//                 answer.add("Fizz");
+//             } else if (divisibleBy5) {
+//                 answer.add("Buzz");
+//             } else {
+//                 answer.add(String.valueOf(i));
+//             }
+//         }
+//         return answer;
+//     }
+
+//     public static void main(String[] args) {
+//         FizzBuzz fb = new FizzBuzz();
+//         List<String> result = fb.fizzBuzz(15);
+
+//         for (String s : result) {
+//             System.out.println(s);
+//         }
+//     }
+// }
+// TC: O(n)
+// SC: O(1)
+
+class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> answer = new ArrayList<>(n);
-
         for (int i = 1; i <= n; i++) {
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
 
-            if (divisibleBy3 && divisibleBy5) {
-                answer.add("FizzBuzz");
-            } else if (divisibleBy3) {
-                answer.add("Fizz");
-            } else if (divisibleBy5) {
-                answer.add("Buzz");
-            } else {
-                answer.add(String.valueOf(i));
+            String currStr = "";
+
+            if (divisibleBy3) {
+                currStr += "Fizz";
             }
+            if (divisibleBy5) {
+                currStr += "Buzz";
+            }
+            if (currStr.isEmpty()) {
+                currStr += String.valueOf(i);
+            }
+            answer.add(currStr);
         }
         return answer;
+        // TC: O(n)
+        // SC: O(1)
     }
 
     public static void main(String[] args) {
@@ -31,5 +68,3 @@ public class FizzBuzz {
         }
     }
 }
-// TC: O(n)
-// SC: O(1)
