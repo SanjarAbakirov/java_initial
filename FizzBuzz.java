9230a07d9c1e0ff8a0e4c36ddabfb616bfadb4ff
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-04-13 23:52:21 +0600

    auto commit Mon Apr 13 23:52:20 +06 2026

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,70 +1,70 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// public class FizzBuzz {
-//     public List<String> fizzBuzz(int n) {
-//         List<String> answer = new ArrayList<>(n);
-
-//         for (int i = 1; i <= n; i++) {
-//             boolean divisibleBy3 = i % 3 == 0;
-//             boolean divisibleBy5 = i % 5 == 0;
-
-//             if (divisibleBy3 && divisibleBy5) {
-//                 answer.add("FizzBuzz");
-//             } else if (divisibleBy3) {
-//                 answer.add("Fizz");
-//             } else if (divisibleBy5) {
-//                 answer.add("Buzz");
-//             } else {
-//                 answer.add(String.valueOf(i));
-//             }
-//         }
-//         return answer;
-//     }
-
-//     public static void main(String[] args) {
-//         FizzBuzz fb = new FizzBuzz();
-//         List<String> result = fb.fizzBuzz(15);
-
-//         for (String s : result) {
-//             System.out.println(s);
-//         }
-//     }
-// }
-// TC: O(n)
-// SC: O(1)
-
 public class FizzBuzz {
     public List<String> fizzBuzz(int n) {
         List<String> answer = new ArrayList<>(n);
+
         for (int i = 1; i <= n; i++) {
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
 
-            String currStr = "";
-
-            if (divisibleBy3) {
-                currStr += "Fizz";
+            if (divisibleBy3 && divisibleBy5) {
+                answer.add("FizzBuzz");
+            } else if (divisibleBy3) {
+                answer.add("Fizz");
+            } else if (divisibleBy5) {
+                answer.add("Buzz");
+            } else {
+                answer.add(String.valueOf(i));
             }
-            if (divisibleBy5) {
-                currStr += "Buzz";
-            }
-            if (currStr.isEmpty()) {
-                currStr += String.valueOf(i);
-            }
-            answer.add(currStr);
         }
         return answer;
-        // TC: O(n)
-        // SC: O(1)
     }
 
     public static void main(String[] args) {
         FizzBuzz fb = new FizzBuzz();
-        List<String> result = fb.fizzBuzz(30);
+        List<String> result = fb.fizzBuzz(15);
 
         for (String s : result) {
             System.out.println(s);
         }
     }
 }
+// TC: O(n)
+// SC: O(1)
+
+// public class FizzBuzz {
+// public List<String> fizzBuzz(int n) {
+// List<String> answer = new ArrayList<>(n);
+// for (int i = 1; i <= n; i++) {
+// boolean divisibleBy3 = i % 3 == 0;
+// boolean divisibleBy5 = i % 5 == 0;
+
+// String currStr = "";
+
+// if (divisibleBy3) {
+// currStr += "Fizz";
+// }
+// if (divisibleBy5) {
+// currStr += "Buzz";
+// }
+// if (currStr.isEmpty()) {
+// currStr += String.valueOf(i);
+// }
+// answer.add(currStr);
+// }
+// return answer;
+// // TC: O(n)
+// // SC: O(1)
+// }
+
+// public static void main(String[] args) {
+// FizzBuzz fb = new FizzBuzz();
+// List<String> result = fb.fizzBuzz(30);
+
+// for (String s : result) {
+// System.out.println(s);
+// }
+// }
+// }
