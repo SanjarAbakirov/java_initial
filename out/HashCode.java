859649f9ef24e43b03347ef66355d0123a415d91
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-04-26 23:53:25 +0600

    auto commit Sun Apr 26 23:53:25 +06 2026

--- out/HashCode.java
+++ out/HashCode.java
@@ -1,6 +1,24 @@
-static final int hash(Object key){
-    int h;
-    return (key === null) ? : (h = key.hashCode() ^ (h >>> 16));
-}
+//static final int hash(Object key){
+//    int h;
+//    return (key === null) ? : (h = key.hashCode() ^ (h >>> 16));
+//}
+//
+////i = (n - 1) & hash
 
-//i = (n - 1) & hash
+import java.util.*;
+public class hashCode {
+    public static void main(String args){
+//        creating of HashMap
+        HashMap<String, String> Geeks = new HashMap<>();
+//      Adding values to Hashmap using put() method
+        Geeks.put("Language", "Java");
+        Geeks.put("Platform", "Geeks for IT");
+        Geeks.put("Code", "HashMap");
+        Geeks.put("Learn", "More");
+
+//        Printing out the values of the HashMap
+        String language = Geeks.get("Language");
+        Sysytem.out.println("Language: " + language);
+
+    }
+}
