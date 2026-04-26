f008ae81faa03b18db876e4efce8dc65718f4332
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-04-27 00:09:37 +0600

    auto commit Mon Apr 27 00:09:37 +06 2026

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // public class FizzBuzz {
 //     public List<String> fizzBuzz(int n) {
@@ -65,6 +64,45 @@ public class FizzBuzz {
 
         for (String s : result) {
             System.out.println(s);
+        }
+    }
+
+    public static class hashCode {
+        public static void main(String args){
+    //        creating of HashMap
+            HashMap<String, String> Geeks = new HashMap<>();
+    //      Adding values to Hashmap using put() method
+            Geeks.put("Language", "Java");
+            Geeks.put("Platform", "Geeks for IT");
+            Geeks.put("Code", "HashMap");
+            Geeks.put("Learn", "More");
+
+    //        Printing out the values of the HashMap
+            String language = Geeks.get("Language");
+            System.out.println("Language: " + language);
+
+    //        Using the containsKey() method to check if a key exists in the HashMap
+            boolean containsKey = Geeks.containsKey("Code");
+            System.out.println("Contains key 'Code'? " + containsKey);
+
+    //        Using the containsValue() method to check if a value exists in the HashMap
+            boolean containsValue = Geeks.containsValue("More");
+            System.out.println("Contains Value 'More'? " + containsValue);
+
+    //        Using the keySet() method to retrieve a Set of all keys in the HashMap
+            Set<String> keySet = Geeks.keySet();
+            System.out.println("Key set: " + keySet);
+
+    //        Using the values() method to retrive a Collection of all values in the HashMap
+            Set<Map.Entry<String, String>> entrySet = Geeks.entrySet();
+            System.out.println("Entry Set: " + entrySet);
+
+    //        Using the remove() method to remove a key-value pair from the HasMap
+            System.out.println("hashmap contents after clear(): " + Geeks);
+
+    //        Using the isEmpty method toi check if the HashMap is empty
+            boolean isEmpty = Geeks.isEmpty();
+            System.out.println("is Empty? " + isEmpty);
         }
     }
 }
