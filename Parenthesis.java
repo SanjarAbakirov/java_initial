8b87c17b65b651e47cb7041f25e5d4b38ec870a2
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-05-13 07:34:38 +0600

    auto commit Wed May 13 07:34:38 +06 2026

--- Parenthesis.java
+++ Parenthesis.java
@@ -5,6 +5,9 @@ public class Parenthesis {
         Stack<Character> stack = new Stack<Character>();
         for (int i=0; i < s.length(); i++){
             char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '['){
+                stack.push();
+            }
         }
     }
 }
