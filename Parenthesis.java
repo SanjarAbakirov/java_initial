0034818b954862183590a6aa2c80c7cb1d07f35a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-05-13 07:48:41 +0600

    auto commit Wed May 13 07:48:41 +06 2026

--- Parenthesis.java
+++ Parenthesis.java
@@ -3,15 +3,21 @@ import java.util.Stack;
 public class Parenthesis {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
-        for (int i=0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
             if (c == '(' || c == '{' || c == '['){
-                stack.push();
+                stack.push(c);
             } else {
-                if(!stack.isEmpty()){
+                if(stack.isEmpty()){
                     return false;
                 }
-            }
+                char top = stack.peek();
+                if ((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '[')){
+                stack.pop();
+                } else {
+                    return false;
+                }
         }
     }
+    return stack.isEmpty();
 }
