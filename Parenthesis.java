d2bb99b1e2ce5ed77379c2d4953406b0e054379f
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-05-13 07:29:18 +0600

    created parenthesis class

--- Parenthesis.java
+++ Parenthesis.java
@@ -3,5 +3,8 @@ import java.util.Stack;
 public class Parenthesis {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
+        for (int i=0; i < s.length(); i++){
+            char c = s.charAt(i);
+        }
     }
 }
