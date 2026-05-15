569f585ca2cb269b06b5e5a5c849e5f863349acd
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-05-15 21:48:06 +0600

    auto commit Fri May 15 21:48:06 +06 2026

--- PolishNotation.java
+++ PolishNotation.java
@@ -4,5 +4,12 @@ public class PolishNotation {
     public int evalRPN(String[] tokens){
         Stack<Integer> stack = new Stack<>();
 
+        for(String token : tokens){
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+
+            }
+        }
     }
 }
