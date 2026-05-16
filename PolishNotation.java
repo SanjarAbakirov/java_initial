0f94eadcc1efbecabfd509457b623883b82a6006
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-05-16 22:39:13 +0600

    auto commit Sat May 16 22:39:13 +06 2026

--- PolishNotation.java
+++ PolishNotation.java
@@ -1,21 +1,21 @@
 import java.util.Stack;
 
 public class PolishNotation {
-    public int evalRPN(String[] tokens){
+    public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
 
-        for(String token : tokens){
+        for (String token : tokens) {
             if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
                 int b = stack.pop();
                 int a = stack.pop();
 
-                if(token.equals("+")){
+                if (token.equals("+")) {
                     stack.push(a + b);
-                } else if(token.equals("-")){
+                } else if (token.equals("-")) {
                     stack.push(a - b);
-                } else if(token.equals("*")){
+                } else if (token.equals("*")) {
                     stack.push(a * b);
-                } else if(token.equals("/")){
+                } else if (token.equals("/")) {
                     stack.push(a / b);
                 }
             } else {
@@ -23,5 +23,14 @@ public class PolishNotation {
             }
         }
         return stack.pop();
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+            Solution solution = new Solution();
+            String[] tokens1 = {"2", "1", "+", "3", "*"};
+            int result1 = solution.evalRPN(tokens1);
+            System.out.println("Example 1:" + String.join(" ", tokens1) + "=" + result1);
+        }
     }
 }
