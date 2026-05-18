8e60e29ffd9721d08c05eebfa206fcea41775f60
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-05-18 23:48:38 +0600

    auto commit Mon May 18 23:48:38 +06 2026

--- PolishNotation.java
+++ PolishNotation.java
@@ -23,14 +23,19 @@ public class PolishNotation {
         }
         return stack.pop();
     }
-}
-
-class Main {
         public static void main(String[] args) {
             PolishNotation polishNotation = new PolishNotation();
+
             String[] tokens1 = {"2", "1", "+", "3", "*"};
             int result1 = polishNotation.evalRPN(tokens1);
-            System.out.println("Example 1:" + String.join(" ", tokens1) + "=" + result1);
+            System.out.println("Example1: " + String.join(" ", tokens1) + " = " + result1);
+
+            String[] tokens2 = {"4", "13", "5", "/", "+"};
+            int result2 = polishNotation.evalRPN(tokens2);
+            System.out.println("Пример 2: " + String.join(" ", tokens2) + " = " + result2);
+
+            String[] tokens3 = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+            int result3 = polishNotation.evalRPN(tokens3);
+            System.out.println("Пример 3: " + String.join(" ", tokens3) + " = " + result3);
         }
 }
-
