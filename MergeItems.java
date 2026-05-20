16b4ef873e497ac4d704cb7f67ed9699b2fb9a2b
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-05-20 23:25:13 +0600

    auto commit Wed May 20 23:25:13 +06 2026

--- MergeItems.java
+++ MergeItems.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class MergeItems {
     public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+                Map<Integer, Integer> map = new HashMap<>();
+
+                for (int[] item : items1){
+                    map.put(item[0], item[1]);
+                }
+                for (int[] item : items2){
+                    map.merge(item[0], item[1], Integer :: sum);
+                }
+
+                List<List<Integer>> result = new ArrayList<>();
+                for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+                    result.add(List.of(entry.getKey(), entry.getValue()));
+                }
+                result.sort(Comparator.comparing(list -> list.get(0)));
+
+                return result;
+
+
 
     }
 }
