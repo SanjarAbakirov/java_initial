93a2cd7dc32092427105f0b6e8111daa5ace5c6c
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-05-22 22:58:08 +0600

    auto commit Fri May 22 22:58:08 +06 2026

--- MergeItems.java
+++ MergeItems.java
@@ -18,8 +18,39 @@ public class MergeItems {
                 result.sort(Comparator.comparing(list -> list.get(0)));
 
                 return result;
-
-
-
     }
+
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MergeItems mergeItems = new MergeItems();
+
+        // Ввод первого массива
+        System.out.println("Введите количество элементов в первом массиве:");
+        int n1 = scanner.nextInt();
+        int[][] items1 = new int[n1][2];
+        System.out.println("Введите пары (значение, вес):");
+        for (int i = 0; i < n1; i++) {
+            items1[i][0] = scanner.nextInt();
+            items1[i][1] = scanner.nextInt();
+        }
+
+        // Ввод второго массива
+        System.out.println("Введите количество элементов во втором массиве:");
+        int n2 = scanner.nextInt();
+        int[][] items2 = new int[n2][2];
+        System.out.println("Введите пары (значение, вес):");
+        for (int i = 0; i < n2; i++) {
+            items2[i][0] = scanner.nextInt();
+            items2[i][1] = scanner.nextInt();
+        }
+
+        // Вызов метода и вывод результата
+        List<List<Integer>> result = mergeItems.mergeSimilarItems(items1, items2);
+        System.out.println("\nРезультат объединения:");
+        for (List<Integer> item : result) {
+            System.out.println("[" + item.get(0) + ", " + item.get(1) + "]");
+        }
+        scanner.close();
+    }
+
 }
